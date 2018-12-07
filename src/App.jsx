6016bbb92904213719bba6d82d8338b4d59d7eff
Ron Khitin <ron@android-ea96e46bbca52265.localdomain>
2018-12-07 12:06:18 +0300

    Setup webpack config

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { styled } from "linaria/react";
+
+const Wrapper = styled.div`
+  background-color: red;
+`;
+
+const App = () => <Wrapper>Hello6</Wrapper>;
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: __dirname + "/src/index.js",
+  output: {
+    path: __dirname + "/dist"
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader"
+          },
+          {
+            loader: "linaria/loader",
+            options: {
+              sourceMap: process.env.NODE_ENV !== "production"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          // [
+          // { loader: "css-hot-loader" },
+          "css-hot-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader"
+          // {
+          //   loader: "css-loader",
+          //   options: {
+          //     sourceMap: process.env.NODE_ENV !== "production"
+          //   }
+          // }
+          // ],
+          // MiniCssExtractPlugin.loader,
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./static/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css"
+    })
+  ],
+  devServer: {
+    hot: true,
+    contentBase: path.join(__dirname, "dist"),
+    compress: false,
+    port: 1234
+  }
+};
